c8b09b1a38e71c9b2f617a2b5704a099b2847c2e
alicedotjpg <alice@thank-etc-ok.com>
2018-04-09 08:18:08 +1000

    Update initialise.js
    Add change function option to examples

--- initialise.js
+++ initialise.js
@@ -1,14 +1,17 @@
  $('.fast').fadingSlideshow({
     intervalTimer: 400,
     fadingTimer: 200,
+    changeFunction: function()
   });
 
   $('.medium').fadingSlideshow({
     intervalTimer: 2000,
     fadingTimer: 1000,
+    changeFunction: function()
   });
 
   $('.slow').fadingSlideshow({
     intervalTimer: 4000,
     fadingTimer: 2000,
+    changeFunction: function()
   });
