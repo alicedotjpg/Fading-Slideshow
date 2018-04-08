f161269ae1864f0aac41d0f14cab63af5e978bc5
alicedotjpg <alice@thank-etc-ok.com>
2018-04-09 08:12:58 +1000

    Create initialise.js

---
+++ initialise.js
@@ -0,0 +1,18 @@
+  /* 
+  Initialise Fading Slideshow on the container that holds the slides.
+  */
+  
+  $('.fast').fadingSlideshow({
+    intervalTimer: 400,
+    fadingTimer: 200,
+  });
+
+  $('.medium').fadingSlideshow({
+    intervalTimer: 2000,
+    fadingTimer: 1000,
+  });
+
+  $('.slow').fadingSlideshow({
+    intervalTimer: 4000,
+    fadingTimer: 2000,
+  });
