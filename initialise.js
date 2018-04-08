7e8eba4ec2330f1156be2349477f94ffe8401291
alicedotjpg <alice@thank-etc-ok.com>
2018-04-09 08:24:08 +1000

    Update initialise.js
    Add example arguments to change function

--- initialise.js
+++ initialise.js
@@ -1,17 +1,17 @@
  $('.fast').fadingSlideshow({
     intervalTimer: 400,
     fadingTimer: 200,
-    changeFunction: function()
+    changeFunction: function(counter, total)
   });
 
   $('.medium').fadingSlideshow({
     intervalTimer: 2000,
     fadingTimer: 1000,
-    changeFunction: function()
+    changeFunction: function(counter, total)
   });
 
   $('.slow').fadingSlideshow({
     intervalTimer: 4000,
     fadingTimer: 2000,
-    changeFunction: function()
+    changeFunction: function(counter, total)
   });
